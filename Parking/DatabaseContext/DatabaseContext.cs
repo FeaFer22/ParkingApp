9fc47d6a0bcf860db18d76647c48ac438e8df231
Soleev Maksim <msoleev98@gmail.com>
2023-04-14 20:07:39 +0300

    ef init

--- Parking/DatabaseContext/DatabaseContext.cs
+++ Parking/DatabaseContext/DatabaseContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ParkingApp.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ParkingApp.DatabaseContext
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<ParkingSlot> ParkingSlots { get; set; } = null!;
+        public DbSet<FixedSlot> FixedSlots { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
         public DatabaseContext() : base() { }
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- Parking/Models/FixedSlot.cs
+++ Parking/Models/FixedSlot.cs
@@ -2,6 +2,10 @@
 {
     public class FixedSlot
     {
+        public Guid Id { get; set; }
+        public DateTime FixationTime { get; set; }
 
+        public ParkingSlot ParkingSlot { get; set; }
+        public User User { get; set; }
     }
 }
